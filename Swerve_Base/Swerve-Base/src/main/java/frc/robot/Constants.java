343d5fd10d85428dea1be713fe30641d7af6f291
Omegabytes-Programmers <omegabytesprogrammers@gmail.com>
2022-01-27 15:13:43 -0500

    Initial Push
    Contains Swerve base robot

---
+++ Swerve_Base/Swerve-Base/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    //Drive Train
+    public static int flsmPort = 0;
+    public static int fldmPort = 1;
+    public static int frsmPort = 2;
+    public static int frdmPort = 3;
+    public static int rrsmPort = 4;
+    public static int rrdmPort = 5;
+    public static int rlsmPort = 6;
+    public static int rldmPort = 7;
+
+    public static int flePort = 0;
+    public static int frePort = 1;
+    public static int rrePort = 2;
+    public static int rlePort = 3;
+
+    public static double fleo = -0.3;
+    public static double freo = -0.45;
+    public static double rreo = 0.4;
+    public static double rleo = 0.28;
+
+    public static int talonCount = 8;
+
+    public static double wheelBase = 14.0;
+
+
+    public static XboxController driveController = new XboxController(0);
+
+
+}
---
+++ Swerve_Base/Swerve-Base/src/main/java/frc/robot/commands/DriveManuallyCommand.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class DriveManuallyCommand extends CommandBase {
+  private final DriveSubsystem drivetrain;
+
+  public DriveManuallyCommand(DriveSubsystem drivetrain){
+      this.drivetrain = drivetrain;
+      addRequirements(drivetrain);
+
+      
+  }
+
+  @Override
+  public void execute() {
+      double translationXPercent = Constants.driveController.getRawAxis(1);
+      double translationYPercent = Constants.driveController.getRawAxis(0);
+      double rotationPercent = Constants.driveController.getRawAxis(4);
+
+      drivetrain.drive(
+              ChassisSpeeds.fromFieldRelativeSpeeds(
+                      translationXPercent * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                      translationYPercent * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                      rotationPercent * DriveSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+                      drivetrain.getRotation()
+              )
+      );
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+      // Stop the drivetrain
+      drivetrain.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+  }
+}
+
