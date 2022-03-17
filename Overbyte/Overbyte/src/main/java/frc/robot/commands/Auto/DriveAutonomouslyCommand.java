85ec564c04fd4a88fedd248a215e1d9b10c99989
Omegabytes-Programmers <omegabytesprogrammers@gmail.com>
2022-03-17 00:51:13 -0400

    Restore the original turn timeout

--- Overbyte/Overbyte/src/main/java/frc/robot/commands/Auto/DriveAutonomouslyCommand.java
+++ Overbyte/Overbyte/src/main/java/frc/robot/commands/Auto/DriveAutonomouslyCommand.java
@@ -78,7 +78,7 @@ public class DriveAutonomouslyCommand extends CommandBase {
 
     currentX = currentPose.getTranslation().getX();
     currentY = currentPose.getTranslation().getY();
-
+s
     currentRotation = currentPose.getRotation().getRadians();
     
     translationXPercent = 0.0 * translationXController.calculate(currentX);
@@ -120,6 +120,6 @@ public class DriveAutonomouslyCommand extends CommandBase {
     }else{
       inPlace = 0;
     }
-    return inPlace > 5 || timeoutTimer.get() > 5;
+    return inPlace > 5 || timeoutTimer.get() > 2.5;
   }
 }
